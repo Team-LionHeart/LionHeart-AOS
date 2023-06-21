b196aa20ec0ce4bde7488d39f51b28d8f199a4a6
watermin <tnals08019@gmail.com>
2023-06-22 01:29:44 +0900

    init : 프로젝트 생성

---
+++ app/src/main/java/com/lionheart/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lionheart
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.lionheart.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+}
