e7dad5693a59d24fd5fdb041b18cb0499719ec94
watermin <tnals08019@gmail.com>
2023-07-07 00:27:15 +0900

    #1 init : activity, fragment 데이터바인딩 추상화

---
+++ app/src/main/java/com/lionheart/core/binding/BindingFragment.kt
@@ -0,0 +1,45 @@
+package com.lionheart.core.binding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BindingFragment<Binding : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int,
+) : Fragment() {
+    private var _binding: Binding? = null
+    protected val binding get() = requireNotNull(_binding) { "value of _binding is null on ${this::class.java.simpleName}" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        constructLayout()
+        addListeners()
+        addObservers()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    open fun constructLayout() {}
+    open fun addListeners() {}
+    open fun addObservers() {}
+}
