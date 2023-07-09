4514ef36ad025f6127c99e8a558708c1003a4992
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-09 16:58:46 +0900

    #4 init : ui 예시 코드

--- app/src/main/java/com/lionheart/presentation/course/WeeklyCourseActivity.kt
+++ app/src/main/java/com/lionheart/presentation/course/WeeklyCourseActivity.kt
@@ -1,12 +1,13 @@
 package com.lionheart.presentation.course
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
+import androidx.activity.viewModels
 import com.lionheart.R
 import com.lionheart.core.binding.BindingActivity
 import com.lionheart.databinding.ActivityWeeklyCourseBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class WeeklyCourseActivity : BindingActivity<ActivityWeeklyCourseBinding>(R.layout.activity_weekly_course) {
-
+@AndroidEntryPoint
+class WeeklyCourseActivity :
+    BindingActivity<ActivityWeeklyCourseBinding>(R.layout.activity_weekly_course) {
+    private val viewModel by viewModels<WeeklyCourseViewModel>()
 }
