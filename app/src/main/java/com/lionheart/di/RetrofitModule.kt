207b43add2f8f0657762465d68cce968e88368ed
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-09 16:58:26 +0900

    #4 init : hilt module 예시 코드

---
+++ app/src/main/java/com/lionheart/di/RetrofitModule.kt
@@ -0,0 +1,68 @@
+package com.lionheart.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.lionheart.BuildConfig.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    private const val APPLICATION_JSON = "application/json"
+
+    @Provides
+    @Singleton
+    fun providesInterceptor(): Interceptor =
+        Interceptor { chain ->
+            with(chain) {
+                proceed(
+                    request()
+                        .newBuilder()
+                        // Header 추가
+                        .build(),
+                )
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        interceptor: Interceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                },
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesExampleRetrofit(
+        client: OkHttpClient,
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                Json.asConverterFactory(
+                    APPLICATION_JSON.toMediaType(),
+                ),
+            )
+            .client(client)
+            .build()
+}
