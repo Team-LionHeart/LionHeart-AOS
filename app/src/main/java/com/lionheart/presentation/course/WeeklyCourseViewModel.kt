1259f036b749975bdf6157a4270a869d8fd7306b
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-09 16:58:46 +0900

    #4 init : ui 예시 코드

--- app/src/main/java/com/lionheart/presentation/course/WeeklyCourseViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/course/WeeklyCourseViewModel.kt
@@ -1,6 +1,32 @@
 package com.lionheart.presentation.course
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lionheart.domain.entity.Lion
+import com.lionheart.domain.repository.LionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class WeeklyCourseViewModel: ViewModel() {
+@HiltViewModel
+class WeeklyCourseViewModel @Inject constructor(
+    private val lionRepository: LionRepository
+) : ViewModel() {
+    private val _exampleData = MutableLiveData<Lion>()
+    val exampleData: LiveData<Lion> get() = _exampleData
+
+    fun getExampleData() {
+        viewModelScope.launch {
+            lionRepository.getArticleList(1)
+                .onSuccess { response ->
+                    _exampleData.value = response
+                }
+                .onFailure {
+                    Timber.tag("getExampleDataFailure").d(it)
+                }
+        }
+    }
 }
