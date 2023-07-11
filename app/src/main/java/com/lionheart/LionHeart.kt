35b2822cfc9a5091deedfe42749816b94cb7f6b6
머환임 <udsward@gmail.com>
2023-07-11 22:11:50 +0900

    #2 feat : add Kakao Login Handler

--- app/src/main/java/com/lionheart/LionHeart.kt
+++ app/src/main/java/com/lionheart/LionHeart.kt
@@ -1,6 +1,8 @@
 package com.lionheart
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.lionheart.BuildConfig.KAKAO_API_KEY
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,5 +11,6 @@ class LionHeart: Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        KakaoSdk.init(this, KAKAO_API_KEY)
     }
 }
