da058e57a733014bf2985d9f9486a63b751d5d5a
머환임 <udsward@gmail.com>
2023-07-11 21:41:18 +0900

    #2 ui : add Login Activity

---
+++ app/src/main/java/com/lionheart/presentation/auth/splash/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.lionheart.presentation.auth.splash
+
+import com.lionheart.R
+import com.lionheart.core.binding.BindingActivity
+import com.lionheart.databinding.ActivityLoginBinding
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun constructLayout() {
+        super.constructLayout()
+
+    }
+
+}
