a092fb54c8026c39ab602df87f8b0dbe6a16f68e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-11 21:40:46 +0900

    #9 ui : 데이터 변경

--- app/src/main/java/com/lionheart/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/search/SearchViewModel.kt
@@ -2,16 +2,41 @@ package com.lionheart.presentation.search
 
 import androidx.lifecycle.ViewModel
 import com.lionheart.R
+import com.lionheart.domain.entity.SearchCategory
 
 class SearchViewModel : ViewModel() {
-    val categoryList = listOf<Int>(
-        R.drawable.ic_search_budget,
-        R.drawable.ic_search_physical_change,
-        R.drawable.ic_search_marital_relationship,
-        R.drawable.ic_search_hospital_treatment,
-        R.drawable.ic_search_support_system,
-        R.drawable.ic_search_prenatal_education,
-        R.drawable.ic_search_layette,
-        R.drawable.ic_search_brink_childbirth,
+    val categoryList = listOf<SearchCategory>(
+        SearchCategory(
+            R.drawable.ic_search_budget,
+            "예산",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_physical_change,
+            "신체 변화",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_marital_relationship,
+            "부부 관계",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_hospital_information,
+            "병원 정보",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_support_system,
+            "지원 제도",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_prenatal_education,
+            "태교",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_layette,
+            "아기 용품",
+        ),
+        SearchCategory(
+            R.drawable.ic_search_father_tip,
+            "아빠들의 팁",
+        ),
     )
 }
