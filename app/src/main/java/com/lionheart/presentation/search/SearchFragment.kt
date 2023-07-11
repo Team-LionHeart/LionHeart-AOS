b62d8b8a220e07626c51724566a24aa5463c5079
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-11 21:40:31 +0900

    #9 ui : search title 분리

--- app/src/main/java/com/lionheart/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/lionheart/presentation/search/SearchFragment.kt
@@ -9,7 +9,6 @@ import com.lionheart.databinding.FragmentSearchBinding
 
 class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private val viewModel by viewModels<SearchViewModel>()
-    private var searchCategoryTitleAdapter: SearchCategoryTitleAdapter? = null
     private var searchCategoryAdapter: SearchCategoryAdapter? = null
 
     override fun constructLayout() {
@@ -17,28 +16,15 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
     }
 
     private fun initAdapter() {
-        searchCategoryTitleAdapter = SearchCategoryTitleAdapter()
         searchCategoryAdapter = SearchCategoryAdapter()
 
         searchCategoryAdapter?.submitList(viewModel.categoryList)
 
-        val gridLayoutManager = GridLayoutManager(context, 2)
-
-        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                return if (position == 0) 2 else 1
-            }
-        }
-
-        binding.rvSearchCategory.layoutManager = gridLayoutManager
-
-        binding.rvSearchCategory.adapter =
-            ConcatAdapter(searchCategoryTitleAdapter, searchCategoryAdapter)
+        binding.rvSearchCategory.adapter = searchCategoryAdapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         searchCategoryAdapter = null
-        searchCategoryTitleAdapter
     }
 }
