3d3198f9fff0b10f20bff5cdf70dd360ffa36529
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-11 14:03:16 +0900

    #12 fix : @Inject constructor 추가

--- app/src/main/java/com/lionheart/data/datasource/LionDataSource.kt
+++ app/src/main/java/com/lionheart/data/datasource/LionDataSource.kt
@@ -4,8 +4,9 @@ import com.lionheart.data.model.request.LionRequest
 import com.lionheart.data.model.response.BaseResponse
 import com.lionheart.data.model.response.LionResponse
 import com.lionheart.data.service.LionService
+import javax.inject.Inject
 
-class LionDataSource(
+class LionDataSource @Inject constructor(
     private val lionService: LionService
 ) {
     suspend fun getArticleList(week: Int): BaseResponse<LionResponse> =
--- app/src/main/java/com/lionheart/data/repository/LionRepositoryImpl.kt
+++ app/src/main/java/com/lionheart/data/repository/LionRepositoryImpl.kt
@@ -3,15 +3,15 @@ package com.lionheart.data.repository
 import com.lionheart.data.datasource.LionDataSource
 import com.lionheart.domain.entity.Lion
 import com.lionheart.domain.repository.LionRepository
+import javax.inject.Inject
 
-class LionRepositoryImpl(
-    private val lionDataSource: LionDataSource
-): LionRepository {
+class LionRepositoryImpl @Inject constructor(
+    private val lionDataSource: LionDataSource,
+) : LionRepository {
     // TODO: 예외처리
     override suspend fun getArticleList(week: Int): Result<Lion> =
         runCatching { lionDataSource.getArticleList(week).data!!.toLion() }
 
-    override suspend fun getArticleDetail(articleId: Int): Result<Lion> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getArticleDetail(articleId: Int): Result<Lion> =
+        runCatching { lionDataSource.getArticleList(articleId).data!!.toLion() }
 }
