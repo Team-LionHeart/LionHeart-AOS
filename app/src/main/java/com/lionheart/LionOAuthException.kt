8b05ff376fa52eefc50472bf9b5b62ff5cfcd1ad
머환임 <udsward@gmail.com>
2023-07-11 22:11:50 +0900

    #2 feat : add Kakao Login Handler

---
+++ app/src/main/java/com/lionheart/LionOAuthException.kt
@@ -0,0 +1,4 @@
+package com.lionheart
+
+class LionOAuthException(message: String) : RuntimeException(message = message) {
+}
---
+++ app/src/main/java/com/lionheart/presentation/auth/splash/OAuthHandler.kt
@@ -0,0 +1,22 @@
+package com.lionheart.presentation.auth.splash
+
+import android.content.Context
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
+import com.lionheart.LionOAuthException
+
+object OAuthHandler {
+    fun kakaoLoginOnApp(context: Context, onSuccess: (OAuthToken) -> Unit) {
+        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+            if (error != null) throw LionOAuthException("카카오 로그인 중 발생한 비정상적인 에러 (detail : ${error.message})")
+            onSuccess(token ?: throw LionOAuthException("카카로 로그인 중 토큰이 정상적으로 넘어오지 않았습니다."))
+        }
+    }
+
+    fun kakaoLoginOnWeb(context: Context, onSuccess: (OAuthToken) -> Unit) {
+        UserApiClient.instance.loginWithKakaoAccount(context) { token, error ->
+            if (error != null) throw LionOAuthException("카카오 로그인 중 발생한 비정상적인 에러 (detail : ${error.message})")
+            onSuccess(token ?: throw LionOAuthException("카카로 로그인 중 토큰이 정상적으로 넘어오지 않았습니다."))
+        }
+    }
+}
