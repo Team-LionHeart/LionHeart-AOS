fc1ea6989b5143540394ac45b3f8dbd0cec1289d
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-13 04:36:16 +0900

    #19 feat : 더미 데이터 추가

---
+++ app/src/main/java/com/lionheart/presentation/search/category/SearchDetailViewModel.kt
@@ -0,0 +1,65 @@
+package com.lionheart.presentation.search.category
+
+import androidx.lifecycle.ViewModel
+import com.lionheart.domain.entity.Article
+
+class SearchDetailViewModel : ViewModel() {
+    val mockArticleList = listOf<Article>(
+        Article(
+            "안녕",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕1",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕2",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕3",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕4",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕5",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+        Article(
+            "안녕6",
+            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
+            "안나ㅏ여네앎ㄴ;이ㅏㄹ;ㅁㄴ아ㅣㅗ마너ㅣㅇ포ㅜㅁ나어륨ㄴ아ㅓㅗㄹ휴마ㅓㄴㅇ류피져ㅏㅓㅇㅎ늎ㅊ ㅠ미ㅏㅕㄴㅇㄹ",
+            9,
+            false,
+            listOf("hi", "hello", "bye"),
+        ),
+    )
+}
