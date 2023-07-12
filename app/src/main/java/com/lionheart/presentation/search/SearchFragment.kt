cf2a256e00470cae81507af90a100b2bf4cef808
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-13 04:35:58 +0900

    #19 feat : 카테고리 클릭 시 intent

--- app/src/main/java/com/lionheart/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/lionheart/presentation/search/SearchFragment.kt
@@ -1,11 +1,11 @@
 package com.lionheart.presentation.search
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.GridLayoutManager
 import com.lionheart.R
 import com.lionheart.core.binding.BindingFragment
 import com.lionheart.databinding.FragmentSearchBinding
+import com.lionheart.presentation.search.category.SearchDetailActivity
 
 class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private val viewModel by viewModels<SearchViewModel>()
@@ -16,11 +16,16 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
     }
 
     private fun initAdapter() {
-        searchCategoryAdapter = SearchCategoryAdapter()
+        searchCategoryAdapter = SearchCategoryAdapter { intentToSearchDetail() }
 
         searchCategoryAdapter?.submitList(viewModel.categoryList)
 
         binding.rvSearchCategory.adapter = searchCategoryAdapter
+    }
+
+    private fun intentToSearchDetail() {
+        val intent = Intent(activity, SearchDetailActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
