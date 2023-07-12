7063e5bd9a1dfd3ead821815c328ea8e62a9163e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-13 05:31:08 +0900

    #19 feat : searchCategory 데이터 추가

--- app/src/main/java/com/lionheart/domain/entity/SearchCategory.kt
+++ app/src/main/java/com/lionheart/domain/entity/SearchCategory.kt
@@ -1,6 +1,13 @@
 package com.lionheart.domain.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class SearchCategory(
     val img: Int,
     val name: String,
-)
+    val serverName: String,
+    val mainTitle: String,
+    val subTitle: String,
+): Parcelable
