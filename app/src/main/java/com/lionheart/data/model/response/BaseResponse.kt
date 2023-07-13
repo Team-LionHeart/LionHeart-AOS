11d8f5d652da2022895fc63095258ab0d461864f
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-14 00:58:41 +0900

    #31 fix : BaseReseponse 수정

--- app/src/main/java/com/lionheart/data/model/response/BaseResponse.kt
+++ app/src/main/java/com/lionheart/data/model/response/BaseResponse.kt
@@ -4,8 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<RESULT>(
-    val status: Int,
-    val success: Boolean,
+    val code: String?,
     val message: String?,
     val data: RESULT?
 )
