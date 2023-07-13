269e0bd50a4a484b92a3638a28e3bc78fffa042d
watermin <tnals08019@gmail.com>
2023-07-13 20:19:59 +0900

    #26 feat : 카드 확장 기능 구현

--- app/src/main/java/com/lionheart/presentation/course/CourseFragment.kt
+++ app/src/main/java/com/lionheart/presentation/course/CourseFragment.kt
@@ -1,11 +1,17 @@
 package com.lionheart.presentation.course
 
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.View
+import androidx.annotation.NonNull
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lionheart.R
 import com.lionheart.core.binding.BindingFragment
 import com.lionheart.databinding.FragmentCourseBinding
 import timber.log.Timber
+
 
 class CourseFragment : BindingFragment<FragmentCourseBinding>(R.layout.fragment_course) {
     private val viewModel by viewModels<CourseViewModel>()
