45f9a5fe02d5e6cd4ee038164f4a2817a23e7a3e
watermin <tnals08019@gmail.com>
2023-07-15 18:27:55 +0900

    #27 ui : 임신주수 입력 화면 추가

---
+++ app/src/main/java/com/lionheart/presentation/auth/onboarding/PregnantWeekFragment.kt
@@ -0,0 +1,9 @@
+package com.lionheart.presentation.auth.onboarding
+
+import com.lionheart.R
+import com.lionheart.core.binding.BindingFragment
+import com.lionheart.databinding.FragmentPregnantWeekBinding
+
+class PregnantWeekFragment : BindingFragment<FragmentPregnantWeekBinding>(R.layout.fragment_pregnant_week) {
+
+}
