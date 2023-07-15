120519e324b3ab5afa57e4e3b51e828f329922af
watermin <tnals08019@gmail.com>
2023-07-15 20:06:08 +0900

    #27 ui : 닉네임 입력 화면 추가

---
+++ app/src/main/java/com/lionheart/presentation/auth/onboarding/BabyNameFragment.kt
@@ -0,0 +1,9 @@
+package com.lionheart.presentation.auth.onboarding
+
+import com.lionheart.R
+import com.lionheart.core.binding.BindingFragment
+import com.lionheart.databinding.FragmentBabyNameBinding
+
+class BabyNameFragment : BindingFragment<FragmentBabyNameBinding>(R.layout.fragment_baby_name) {
+
+}
