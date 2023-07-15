33f3d7b4c59f52c49bd5c1ac0fdf7b07728d96eb
watermin <tnals08019@gmail.com>
2023-07-15 18:26:07 +0900

    #27 ui : activity 생성 및 ui 구현

---
+++ app/src/main/java/com/lionheart/presentation/auth/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.lionheart.presentation.auth.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lionheart.R
+import com.lionheart.core.binding.BindingActivity
+import com.lionheart.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity : BindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+
+
+}
