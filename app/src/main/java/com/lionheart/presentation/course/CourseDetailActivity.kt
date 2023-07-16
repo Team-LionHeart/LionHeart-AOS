9a162d7b33f14fc0224560349663158b540ef469
watermin <tnals08019@gmail.com>
2023-07-16 16:18:49 +0900

    #29 chore : CourseWeekly -> CourseDetail

--- app/src/main/java/com/lionheart/presentation/course/CourseDetailActivity.kt
+++ app/src/main/java/com/lionheart/presentation/course/CourseDetailActivity.kt
@@ -2,22 +2,20 @@ package com.lionheart.presentation.course
 
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.lionheart.R
 import com.lionheart.core.binding.BindingActivity
 import com.lionheart.databinding.ActivityCourseWeeklyBinding
-import com.lionheart.domain.entity.CourseWeek
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class CourseWeeklyActivity :
+class CourseDetailActivity :
     BindingActivity<ActivityCourseWeeklyBinding>(R.layout.activity_course_weekly) {
-    private val viewModel by viewModels<CourseWeeklyViewModel>()
-    private var _courseWeeklyTitleAdapter: CourseWeeklyTitleAdapter? = null
+    private val viewModel by viewModels<CourseDetailViewModel>()
+    private var _courseWeeklyTitleAdapter: CourseDetailTitleAdapter? = null
     private val courseWeeklyTitleAdapter
         get() = requireNotNull(_courseWeeklyTitleAdapter) { Timber.e("adapter not initialized") }
-    private var _courseWeeklyAdapter: CourseWeeklyAdapter? = null
+    private var _courseWeeklyAdapter: CourseDetailAdapter? = null
     private val courseWeeklyAdapter
         get() = requireNotNull(_courseWeeklyAdapter) { Timber.e("adapter not initialized") }
 
@@ -25,14 +23,14 @@ class CourseWeeklyActivity :
         // databinding
         with(binding) {
             vm = viewModel
-            lifecycleOwner = this@CourseWeeklyActivity
+            lifecycleOwner = this@CourseDetailActivity
         }
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
-        _courseWeeklyTitleAdapter = CourseWeeklyTitleAdapter(viewModel.tempHeader)
-        _courseWeeklyAdapter = CourseWeeklyAdapter(viewModel.courseWeeklyList)
+        _courseWeeklyTitleAdapter = CourseDetailTitleAdapter(viewModel.tempHeader)
+        _courseWeeklyAdapter = CourseDetailAdapter(viewModel.courseWeeklyList)
         with(binding.rvCourseWeekly) {
             adapter = ConcatAdapter(courseWeeklyTitleAdapter, courseWeeklyAdapter)
         }
--- app/src/main/java/com/lionheart/presentation/course/CourseDetailTitleAdapter.kt
+++ app/src/main/java/com/lionheart/presentation/course/CourseDetailTitleAdapter.kt
@@ -3,29 +3,28 @@ package com.lionheart.presentation.course
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import com.lionheart.databinding.ItemCourseWeeklyHeaderBinding
+import com.lionheart.databinding.ItemCourseDetailHeaderBinding
 import com.lionheart.domain.entity.CourseWeek
 
-class CourseWeeklyTitleAdapter(
+class CourseDetailTitleAdapter(
     private val week: CourseWeek
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemCourseWeeklyHeaderBinding.inflate(inflater, parent, false)
-        return CourseWeeklyTitleViewHolder(binding)
+        val binding = ItemCourseDetailHeaderBinding.inflate(inflater, parent, false)
+        return CourseDetailTitleViewHolder(binding)
     }
 
     override fun getItemCount() = 1
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as CourseWeeklyTitleViewHolder).onBind(week)
+        (holder as CourseDetailTitleViewHolder).onBind(week)
     }
 
 }
 
-class CourseWeeklyTitleViewHolder(private val binding: ItemCourseWeeklyHeaderBinding) :
+class CourseDetailTitleViewHolder(private val binding: ItemCourseDetailHeaderBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(data: CourseWeek) {
         binding.data = data
