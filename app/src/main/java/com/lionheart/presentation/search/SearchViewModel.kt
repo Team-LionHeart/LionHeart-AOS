accdb49eb796a9da2089b15bdff148e5d0b4a697
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-17 18:46:38 +0900

    #19 feat : articleCategory enum class

--- app/src/main/java/com/lionheart/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/search/SearchViewModel.kt
@@ -3,62 +3,63 @@ package com.lionheart.presentation.search
 import androidx.lifecycle.ViewModel
 import com.lionheart.R
 import com.lionheart.domain.entity.SearchCategory
+import com.lionheart.presentation.search.category.ArticleCategory
 
 class SearchViewModel : ViewModel() {
     val categoryList = listOf<SearchCategory>(
         SearchCategory(
             R.drawable.ic_search_budget,
             "예산",
-            "BUDGET",
+            ArticleCategory.BUDGET,
             "출산 준비,\n불필요한 지출은 그만",
             "라이온하트가 전하는 예산 절약 노하우 대공개",
         ),
         SearchCategory(
             R.drawable.ic_search_physical_change,
             "신체 변화",
-            "PHYSICAL_CHANGE",
+            ArticleCategory.PHYSICAL_CHANGE,
             "와이프 신체 변화 미리 알고\n든든한 남편 되기",
             "라이온하트가 전하는 아내의 신체변화에 대한 모든 것",
         ),
         SearchCategory(
             R.drawable.ic_search_marital_relationship,
             "부부 관계",
-            "MARITAL_RELATIONSHIP",
+            ArticleCategory.MARITAL_RELATIONSHIP,
             "똑똑한 아이를 원한다면\n사랑하세요",
             "라이온하트가 전하는 좋은 남편이 되는 방법 대공개",
         ),
         SearchCategory(
             R.drawable.ic_search_hospital_information,
             "병원 정보",
-            "HOSPITAL_INFORMATION",
+            ArticleCategory.HOSPITAL_INFORMATION,
             "산부인과는 조금 낯선\n예비 아빠들에게",
             "라이온하트가 전하는 병원 진료에 대한 모든 것",
         ),
         SearchCategory(
             R.drawable.ic_search_support_system,
             "지원 제도",
-            "SUPPORT_SYSTEM",
+            ArticleCategory.SUPPORT_SYSTEM,
             "모르면 손해,\n임신부 혜택 똑똑하게 챙기기",
             "라이온하트가 전하는 지원 제도에 대한 모든 것",
         ),
         SearchCategory(
             R.drawable.ic_search_prenatal_education,
             "태교",
-            "PRENATAL_EDUCATION",
+            ArticleCategory.PRENATAL_EDUCATION,
             "부담없이 편안하게\n태교하는 법",
             "라이온하트가 전하는 아이를 위하는 태교 노하우",
         ),
         SearchCategory(
             R.drawable.ic_search_layette,
             "아기 용품",
-            "BABY_GOODS",
+            ArticleCategory.BABY_GOODS,
             "미리미리 아기용품 준비해서\n좋은 아빠 되기",
             "라이온하트가 전하는 아기 용품에 대한 모든 것",
         ),
         SearchCategory(
             R.drawable.ic_search_father_tip,
             "아빠들의 팁",
-            "DAD_TIPS",
+            ArticleCategory.DAD_TIPS,
             "선배 아빠들의\n진심어린 이야기",
             "라이온하트가 예비아빠들에게 전하는 마음",
         ),
