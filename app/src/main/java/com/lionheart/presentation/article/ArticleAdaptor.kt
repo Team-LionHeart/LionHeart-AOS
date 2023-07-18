d71fda7fb220a514cef07c77e2e95beb531859ee
머환임 <udsward@gmail.com>
2023-07-18 13:11:15 +0900

    리뷰 반영

--- app/src/main/java/com/lionheart/presentation/article/ArticleAdaptor.kt
+++ app/src/main/java/com/lionheart/presentation/article/ArticleAdaptor.kt
@@ -1,7 +1,6 @@
 package com.lionheart.presentation.article
 
 import android.content.Context
-import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -19,63 +18,49 @@ class ArticleAdaptor : ListAdapter<ArticleComponent, ArticleViewHolder>(ItemDiff
     onItemsTheSame = { old, new -> old == new },
     onContentsTheSame = { old, new -> old == new }
 )) {
-    private lateinit var inflater: LayoutInflater
-
     override fun getItemViewType(position: Int): Int = getItem(position).type
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        inflater = parent.context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
         when (viewType) {
-            ArticleComponentType.EDITOR_NOTE.id -> {
-                return EditorNoteViewHolder(
-                    ItemArticleEditorNoteBinding.inflate(
-                        layoutInflaterFromParent(parent.context),
-                        parent,
-                        false
-                    )
+            ArticleComponentType.EDITOR_NOTE.id -> return EditorNoteViewHolder(
+                ItemArticleEditorNoteBinding.inflate(
+                    layoutInflaterFromParent(parent.context),
+                    parent,
+                    false
                 )
-            }
+            )
 
-            ArticleComponentType.CHAPTER_TITLE.id -> {
-                return ChapterTitleViewHolder(
-                    ItemArticleChaptorTitleBinding.inflate(
-                        layoutInflaterFromParent(parent.context),
-                        parent,
-                        false
-                    )
+            ArticleComponentType.CHAPTER_TITLE.id -> return ChapterTitleViewHolder(
+                ItemArticleChaptorTitleBinding.inflate(
+                    layoutInflaterFromParent(parent.context),
+                    parent,
+                    false
                 )
-            }
+            )
 
-            ArticleComponentType.BODY.id -> {
-                return BodyViewHolder(
-                    ItemArticleBodyBinding.inflate(
-                        layoutInflaterFromParent(parent.context),
-                        parent,
-                        false
-                    )
+            ArticleComponentType.BODY.id -> return BodyViewHolder(
+                ItemArticleBodyBinding.inflate(
+                    layoutInflaterFromParent(parent.context),
+                    parent,
+                    false
                 )
-            }
+            )
 
-            ArticleComponentType.GENERAL_TITLE.id -> {
-                return GeneralTitleViewHolder(
-                    ItemArticleGeneralTitleBinding.inflate(
-                        layoutInflaterFromParent(parent.context),
-                        parent,
-                        false
-                    )
+            ArticleComponentType.GENERAL_TITLE.id -> return GeneralTitleViewHolder(
+                ItemArticleGeneralTitleBinding.inflate(
+                    layoutInflaterFromParent(parent.context),
+                    parent,
+                    false
                 )
-            }
+            )
 
-            ArticleComponentType.IMAGE.id -> {
-                return ImageViewHolder(
-                    ItemArticleImageBinding.inflate(
-                        layoutInflaterFromParent(
-                            parent.context
-                        ), parent, false
-                    )
+            ArticleComponentType.IMAGE.id -> return ImageViewHolder(
+                ItemArticleImageBinding.inflate(
+                    layoutInflaterFromParent(
+                        parent.context
+                    ), parent, false
                 )
-            }
+            )
 
             else -> throw LionUnKnownException("알 수 없는 오류가 발생헀습니다.")
         }
