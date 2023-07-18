3086d18953440085e2c9f5104868e5ecfc1d8085
머환임 <udsward@gmail.com>
2023-07-18 21:00:11 +0900

    #23 ui: add today fragment

---
+++ app/src/main/java/com/lionheart/domain/entity/TodayCard.kt
@@ -0,0 +1,7 @@
+package com.lionheart.domain.entity
+
+data class TodayCard(
+    val week: String,
+    val title: String,
+    val detail: String,
+)
---
+++ app/src/main/java/com/lionheart/domain/entity/User.kt
@@ -0,0 +1,5 @@
+package com.lionheart.domain.entity
+
+data class User(
+    val nickname: String
+)
