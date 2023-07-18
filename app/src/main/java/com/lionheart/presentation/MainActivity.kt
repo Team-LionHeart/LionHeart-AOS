20ed78e2fae1895638a515817c6e8cf091768ace
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-18 14:43:36 +0900

    #44 feat : bookmark 클릭 시 intent

--- app/src/main/java/com/lionheart/presentation/MainActivity.kt
+++ app/src/main/java/com/lionheart/presentation/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.lionheart.presentation
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import android.content.Intent
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.lionheart.R
 import com.lionheart.core.binding.BindingActivity
 import com.lionheart.databinding.ActivityMainBinding
-import com.lionheart.presentation.search.SearchFragment
+import com.lionheart.presentation.bookmark.BookMarkActivity
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var navController: NavController
@@ -25,10 +16,21 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         initBottomNavigation()
     }
 
+    override fun addListeners() {
+        intentToBookmark()
+    }
+
     private fun initBottomNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         navController = navHostFragment.navController
         binding.bnvMain.setupWithNavController(navController)
+    }
+
+    private fun intentToBookmark() {
+        val intent = Intent(this, BookMarkActivity::class.java)
+        binding.ivMainBookmark.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
