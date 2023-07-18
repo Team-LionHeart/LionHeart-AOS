4b753900f5ec486df174386d4b4f7882c4d2b377
watermin <tnals08019@gmail.com>
2023-07-18 23:05:31 +0900

    #27 feat : progress bar 추가, 이전 다음 버튼 로직 수정

--- app/src/main/java/com/lionheart/presentation/auth/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/auth/onboarding/OnboardingViewModel.kt
@@ -4,13 +4,4 @@ import androidx.lifecycle.ViewModel
 
 class OnboardingViewModel: ViewModel() {
     var step: Int = 0
-
-    fun goToNext(): Int {
-        if(step < 2) { step++ }
-        return step
-    }
-    fun goToPrevious(): Int {
-        if(step > 0) { step-- }
-        return step
-    }
 }
