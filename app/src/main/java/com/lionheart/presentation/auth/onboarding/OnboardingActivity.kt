14ed333edf1ab61336db0f6b9dd672e59b241a7e
watermin <tnals08019@gmail.com>
2023-07-18 23:05:31 +0900

    #27 feat : progress bar 추가, 이전 다음 버튼 로직 수정

--- app/src/main/java/com/lionheart/presentation/auth/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/lionheart/presentation/auth/onboarding/OnboardingActivity.kt
@@ -13,22 +13,39 @@ import timber.log.Timber
 class OnboardingActivity :
     BindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
     private val viewModel by viewModels<OnboardingViewModel>()
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fcv_onboarding) as NavHostFragment }
 
     override fun addListeners() {
+        navHostFragment.navController.addOnDestinationChangedListener { _, dest, _ ->
+            when (dest.label) {
+                "pregnant_week_fragment" -> {
+                    with (binding) {
+                        progressOnboarding.progress = 50
+                        btnOnboardingNext.tag = 0
+                        btnOnboardingBack.tag = 0
+                    }
+                }
+                "baby_name_fragment" -> {
+                    with(binding) {
+                        progressOnboarding.progress = 100
+                        btnOnboardingNext.tag = 1
+                        btnOnboardingBack.tag = 1
+                    }
+                }
+                else -> binding.progressOnboarding.progress = 0
+            }
+        }
         binding.btnOnboardingNext.setOnClickListener {
             when (it.tag) {
                 // 임신 주수 뷰에서
                 0 -> {
                     findNavController(R.id.fcv_onboarding).navigate(R.id.action_pregnant_week_fragment_to_baby_name_fragment)
-                    it.tag = viewModel.goToNext()
-                    Timber.d("next : 임신 주수 -> 태명 / ${it.tag}")
                 }
                 // 태명 뷰에서
                 1 -> {
                     Intent(this, RegisterDoneActivity::class.java).apply {
                         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     }.run(::startActivity)
-                    Timber.d("next : 태명 -> 완료! / ${it.tag}")
                 }
             }
         }
@@ -40,14 +57,10 @@ class OnboardingActivity :
                     Intent(this, LoginActivity::class.java).apply {
                         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     }.run(::startActivity)
-                    Timber.d("back : 임신 주수 -> 로그인 / ${it.tag}")
                 }
                 // 태명 뷰에서
                 1 -> {
                     findNavController(R.id.fcv_onboarding).navigate(R.id.action_baby_name_fragment_to_pregnant_week_fragment)
-                    it.tag = viewModel.goToPrevious()
-                    binding.btnOnboardingNext.tag = it.tag
-                    Timber.d("back : 태명 -> 임신 주수 / ${it.tag}")
                 }
             }
         }
