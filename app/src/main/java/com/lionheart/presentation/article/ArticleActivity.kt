0056dfd166cf1daee2d592f33bd5e92af62ad4b5
머환임 <udsward@gmail.com>
2023-07-18 13:17:58 +0900

    #24 ui: add go top floating button on article activity

--- app/src/main/java/com/lionheart/presentation/article/ArticleActivity.kt
+++ app/src/main/java/com/lionheart/presentation/article/ArticleActivity.kt
@@ -1,5 +1,6 @@
 package com.lionheart.presentation.article
 
+import android.view.View
 import com.lionheart.R
 import com.lionheart.core.binding.BindingActivity
 import com.lionheart.databinding.ActivityArticleBinding
@@ -8,10 +9,10 @@ class ArticleActivity : BindingActivity<ActivityArticleBinding>(R.layout.activit
     private lateinit var adaptor: ArticleAdaptor
     override fun constructLayout() {
         setAdaptor()
-        setProgressBar()
     }
 
     override fun addListeners() {
+        listenScroll()
     }
 
     private fun setAdaptor() {
@@ -22,11 +23,26 @@ class ArticleActivity : BindingActivity<ActivityArticleBinding>(R.layout.activit
 
     private fun getArticleComponents() = ArticleMocker.mockChapter1()
 
-    private fun setProgressBar() {
+    private fun listenScroll() {
         binding.layoutArticleScroll.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
             val maxScroll =
                 binding.layoutArticleScroll.getChildAt(0).height - binding.layoutArticleScroll.height
             binding.progressArticleTop.progress = ((scrollY.toFloat() / maxScroll) * 100).toInt()
+
+            if (scrollY <= oldScrollY) { // on down Scroll
+                binding.fbtnArticleGoTop.visibility = View.VISIBLE
+            } else { // on up Scroll
+                binding.fbtnArticleGoTop.visibility = View.GONE
+            }
+
+            if (scrollY == 0) {
+                binding.fbtnArticleGoTop.visibility = View.GONE
+            }
+        }
+
+        binding.fbtnArticleGoTop.setOnClickListener {
+            binding.layoutArticleScroll.scrollY = 0
+            binding.fbtnArticleGoTop.visibility = View.GONE
         }
     }
 }
