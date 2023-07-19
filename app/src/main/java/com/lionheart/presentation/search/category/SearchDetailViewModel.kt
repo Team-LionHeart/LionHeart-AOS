9cffc206645bf4c8f724901cc8895cbd34e5aa17
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-19 17:55:06 +0900

    #58 feat : articlelist 불러오기

--- app/src/main/java/com/lionheart/presentation/search/category/SearchDetailViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/search/category/SearchDetailViewModel.kt
@@ -1,9 +1,29 @@
 package com.lionheart.presentation.search.category
 
+import android.accounts.NetworkErrorException
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lionheart.LionUnKnownException
+import com.lionheart.core.uistate.UiState
 import com.lionheart.domain.entity.Article
+import com.lionheart.core.uistate.UiState.Failure
+import com.lionheart.core.uistate.UiState.Success
+import com.lionheart.domain.entity.ArticleCategory
+import com.lionheart.domain.repository.ArticleRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
 
-class SearchDetailViewModel : ViewModel() {
+@HiltViewModel
+class SearchDetailViewModel @Inject constructor(
+    private val articleRepository: ArticleRepository,
+) : ViewModel() {
     val mockArticleList = listOf<Article>(
         Article(
             "안녕",
@@ -62,4 +82,35 @@ class SearchDetailViewModel : ViewModel() {
             listOf("hi", "hello", "bye"),
         ),
     )
+
+    private val _getCategoryArticleState = MutableSharedFlow<UiState<List<Article>>>()
+    val getCategoryArticleState = _getCategoryArticleState.asSharedFlow()
+
+    fun getCategoryArticle(category: ArticleCategory) {
+        viewModelScope.launch {
+            articleRepository.getArticleByCategory(category)
+                .catch { error ->
+                    when (error) {
+                        is HttpException -> {
+                            _getCategoryArticleState.emit(Failure(error.code()))
+                        }
+
+                        is NetworkErrorException -> {
+                            _getCategoryArticleState.emit(Failure(NETWORK_ERROR))
+                        }
+
+                        else -> {
+                            LionUnKnownException("알 수 없는 문제가 발생하였습니다")
+                        }
+                    }
+                }
+                .collect { response ->
+                    _getCategoryArticleState.emit(Success(response))
+                }
+        }
+    }
+
+    companion object {
+        val NETWORK_ERROR = 0
+    }
 }
