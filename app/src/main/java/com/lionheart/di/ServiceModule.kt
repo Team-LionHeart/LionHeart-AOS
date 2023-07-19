c9672e079eda49b03bc87ad938936852acb8735a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-19 17:26:46 +0900

    #56 feat : module 추가

--- app/src/main/java/com/lionheart/di/ServiceModule.kt
+++ app/src/main/java/com/lionheart/di/ServiceModule.kt
@@ -1,14 +1,43 @@
 package com.lionheart.di
 
+import com.lionheart.data.service.ArticleService
+import com.lionheart.data.service.AuthService
+import com.lionheart.data.service.ChallengeService
+import com.lionheart.data.service.CurriculumService
+import com.lionheart.data.service.MemberService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
+    @Provides
+    @Singleton
+    fun articleService(retrofit: Retrofit): ArticleService =
+        retrofit.create(ArticleService::class.java)
 
+    @Provides
+    @Singleton
+    fun authService(retrofit: Retrofit): AuthService =
+        retrofit.create(AuthService::class.java)
+
+    @Provides
+    @Singleton
+    fun challengeService(retrofit: Retrofit): ChallengeService =
+        retrofit.create(ChallengeService::class.java)
+
+    @Provides
+    @Singleton
+    fun curriculumService(retrofit: Retrofit): CurriculumService =
+        retrofit.create(CurriculumService::class.java)
+
+    @Provides
+    @Singleton
+    fun memberService(retrofit: Retrofit): MemberService =
+        retrofit.create(MemberService::class.java)
 }
