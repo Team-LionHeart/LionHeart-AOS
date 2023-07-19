638e50aa4e26530519983710ce5431676f61f9aa
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-20 05:31:40 +0900

    #63 feat : bookmarkList data 부분 구현

--- app/src/main/java/com/lionheart/domain/entity/BookmarkArticle.kt
+++ app/src/main/java/com/lionheart/domain/entity/BookmarkArticle.kt
@@ -1,10 +1,13 @@
 package com.lionheart.domain.entity
 
+import kotlinx.serialization.SerialName
+
 data class BookmarkArticle(
     val articleSummaries: List<ArticleSummary>,
     val babyNickname: String,
 ){
     data class ArticleSummary(
+        val articleId: Long,
         val isMarked: Boolean,
         val mainImageUrl: String,
         val tags: List<String>,
