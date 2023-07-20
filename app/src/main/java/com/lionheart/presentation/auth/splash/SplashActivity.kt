30aae5894d293f6ba3affbd0d5868651cc88739b
머환임 <udsward@gmail.com>
2023-07-20 22:10:15 +0900

    [NO-ISSUE] ui: add lottie on splash

---
+++ app/src/main/java/com/lionheart/presentation/auth/splash/SplashActivity.kt
@@ -0,0 +1,8 @@
+package com.lionheart.presentation.auth.splash
+
+import com.lionheart.R
+import com.lionheart.core.binding.BindingActivity
+import com.lionheart.databinding.ActivitySplashBinding
+
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+}
