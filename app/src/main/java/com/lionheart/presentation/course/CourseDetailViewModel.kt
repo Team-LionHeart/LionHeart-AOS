2f265bd68e281b936c080814371ff86a57c1b603
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-20 20:32:53 +0900

    #69 feat : course submit 구현

--- app/src/main/java/com/lionheart/presentation/course/CourseDetailViewModel.kt
+++ app/src/main/java/com/lionheart/presentation/course/CourseDetailViewModel.kt
@@ -1,82 +1,58 @@
 package com.lionheart.presentation.course
 
+import android.accounts.NetworkErrorException
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lionheart.LionUnKnownException
+import com.lionheart.core.uistate.UiState
+import com.lionheart.core.uistate.UiState.Failure
+import com.lionheart.core.uistate.UiState.Success
 import com.lionheart.domain.entity.Article
 import com.lionheart.domain.entity.CourseWeek
+import com.lionheart.domain.repository.ArticleRepository
+import com.lionheart.presentation.search.category.SearchDetailViewModel.Companion.NETWORK_ERROR
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
-class CourseDetailViewModel @Inject constructor() : ViewModel() {
+class CourseDetailViewModel @Inject constructor(
+    private val articleRepository: ArticleRepository,
+) : ViewModel() {
+    private val _getCourseArticleState = MutableSharedFlow<UiState<List<Article>>>()
+    val getCourseArticleState = _getCourseArticleState.asSharedFlow()
 
     val tempHeader = CourseWeek(
-        week = 28,
-        imageUrl = "https://github.com/gosopt-LionHeart/LionHeart-AOS/assets/74162198/4743f4e7-5323-4230-8db2-ac38df8e926b"
+        week = 6,
+        imageUrl = "https://github.com/gosopt-LionHeart/LionHeart-AOS/assets/74162198/4743f4e7-5323-4230-8db2-ac38df8e926b",
     )
 
-    val courseWeeklyList = listOf(
-        Article(
-            0,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            9,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-        Article(
-            1,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요. 젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            12,
-            false,
-            listOf("신체변화", "태교", "병원 정보"),
-        ),
-        Article(
-            2,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            9,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-        Article(
-            3,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            12,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-        Article(
-            4,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            9,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-        Article(
-            5,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            12,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-        Article(
-            6,
-            "육아카드 만들기 A-Z",
-            "https://github.com/SeonHwan-Kim/VelogInMobile/assets/96679633/53d3618b-2978-47f0-a2fd-8c8f98435047",
-            "젖병을 잘못 구매하면, 배앓이를 한다는 말 들어보셨나요? 오늘은 신생아에게 가장 중요한 젖병 이야기를 들려드릴게요.",
-            9,
-            false,
-            listOf("신체변화", "건강", "병원 정보"),
-        ),
-    )
+    fun getCourseArticle(week: Long) {
+        viewModelScope.launch {
+            articleRepository.getWeeklyArticle(week)
+                .catch { error ->
+                    when (error) {
+                        is HttpException -> {
+                            _getCourseArticleState.emit(Failure(error.code()))
+                        }
+
+                        is NetworkErrorException -> {
+                            _getCourseArticleState.emit(Failure(NETWORK_ERROR))
+                        }
+
+                        else -> {
+                            LionUnKnownException("알 수 없는 에러가 발생하였습니다 : $error")
+                        }
+                    }
+                }
+                .collect { response ->
+                    _getCourseArticleState.emit(Success(response))
+                }
+        }
+    }
 }
