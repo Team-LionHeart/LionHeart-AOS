581de724efcc44a2d2b0b56b5d2cf9bc84a80a37
watermin <tnals08019@gmail.com>
2023-07-21 22:33:46 +0900

    #28 feat : 마이페이지 뷰 구현

---
+++ app/src/main/java/com/lionheart/presentation/mypage/MyPageActivity.kt
@@ -0,0 +1,11 @@
+package com.lionheart.presentation.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lionheart.R
+import com.lionheart.core.binding.BindingActivity
+import com.lionheart.databinding.ActivityMyPageBinding
+
+class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
+
+}
